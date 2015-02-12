110fb32602938bc362521fc28b70baa1fc660877
David Heyman <dave@axismaps.com>
2015-02-12 13:29:43 +0000

    Draw call and zoom buttons

--- main.js
+++ main.js
@@ -1,16 +1,58 @@
 var map,
-	user;
+	user,
+	drawn;
 
 function init() {
 	check_cookie();
 	init_map();
-//	setup_events();
+	init_events();
 	resize();
 }
 
 function init_map() {
-	map = L.map( 'map' ).setView( [ 32.78, -96.8 ], 12 );
+	//Initializing map and tile layer
+	map = L.map( 'map', { zoomControl: false, minZoom : 9 } ).setView( [ 32.78, -96.8 ], 12 );
 	L.tileLayer( tileAddress ).addTo( map );
+	
+	//Setting up drawing
+	drawn = new L.FeatureGroup();
+	map.addLayer( drawn );
+}
+
+function init_events(){
+	$( "#draw-call" ).click( function() {
+		$( this ).animate( { opacity : 0 }, 'fast', function() {
+			$( "#drawing" ).show();
+			$( this ).hide();
+		});
+		
+		return false;
+	});
+	
+	$( "#zoom-out" ).click( function() {
+		map.zoomOut();
+		checkZoom();
+	});
+	$( "#zoom-in" ).click( function() {
+		map.zoomIn();
+		checkZoom();
+	});
+	
+	function checkZoom() {
+		if( map.getZoom() + 1 >= map.getMaxZoom() ) {
+			$( "#zoom-in" ).addClass( "disabled" );
+		}
+		else {
+			$( "#zoom-in" ).removeClass( "disabled" );
+		}
+		
+		if( map.getZoom() - 1 <= map.getMinZoom() ) {
+			$( "#zoom-out" ).addClass( "disabled" );
+		}
+		else {
+			$( "#zoom-out" ).removeClass( "disabled" );
+		}
+	}
 }
 
 function resize() {
