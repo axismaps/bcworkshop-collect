4d745ee48cb889c5514980feb32354d1a7ffc40f
David Heyman <dave@axismaps.com>
2015-02-18 12:29:24 +0000

    Use random colorbrewer color for polygons

--- js/draw.js
+++ js/draw.js
@@ -1,17 +1,24 @@
 var drawn,
-	sketch;
+	sketch,
+	colors = _.shuffle( [ 'rgb(228,26,28)', 'rgb(55,126,184)', 'rgb(77,175,74)', 'rgb(152,78,163)', 'rgb(255,127,0)', 'rgb(255,255,51)','rgb(166,86,40)', 'rgb(247,129,191)', 'rgb(153,153,153)' ] );
 
 function draw_polygon(){
 	clear_sketch();	
 	$( "#polygon-controls" ).css( "display", "inline-block" );
 	
-	sketch = new L.Draw.PolygonTouch( map, {allowIntersection: false } );
+	sketch = new L.Draw.PolygonTouch( map, { 
+		allowIntersection : false,
+		shapeOptions : { color : getColor() }
+	});
 	sketch.enable();
 }
 
 function draw_circle(){
 	clear_sketch();
-	sketch = new L.Draw.Circle( map );
+	sketch = new L.Draw.Circle( map, { 
+		showRadius : false,
+		shapeOptions : { color : getColor() }
+	});
 	sketch.enable();
 }
 
@@ -39,4 +46,10 @@ function finish_draw( e ){
 	$( "#geojson" ).val( geojson );
 	$( "#uuid" ).val( user );
 	$( '#name' ).modal();
+}
+
+function getColor() {
+	var c = colors.shift();
+	colors.push( c );
+	return c;
 }
