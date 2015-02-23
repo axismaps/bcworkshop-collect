b948d2cc0fac8e467bae14ec6ab515f1845a706f
David Heyman <dave@axismaps.com>
2015-02-23 07:14:57 +0000

    Copy layer when moving to holding and drawn

--- js/draw.js
+++ js/draw.js
@@ -47,8 +47,6 @@ function clear_sketch() {
 	$( "#confirm" ).modal( 'hide' );
 }
 
-function finish_draw( e ) {
-	holding = e.layer;
 function delete_neighborhood( name ) {
 	$.ajax({
 		url : endpoint + '/delete/' + user + '/' + encodeURIComponent( name ),
@@ -57,16 +55,20 @@ function delete_neighborhood( name ) {
 		}
 	});
 }
+
+function finish_draw( e ) {	
+	holding = copy_layer( e.layer );
 	holding.addTo( map );
 	
 	if( e.layerType == "circle" ) {
-		var pt = holding.toGeoJSON();
-		var buffer = turf.buffer( pt, holding.getRadius(), "meters" );
+		var pt = e.layer.toGeoJSON();
+		var buffer = turf.buffer( pt, e.layer.getRadius(), "meters" );
 		var geom = buffer.features[ 0 ].geometry;
 	}
 	else {
-		var geom = holding.toGeoJSON().geometry;
+		var geom = e.layer.toGeoJSON().geometry;
 	}
+	
 	var geojson = JSON.stringify( geom ).replace( /\}$/gim, ",\"crs\":{\"type\":\"name\",\"properties\":{\"name\":\"EPSG:4326\"}}}" );
 	$( "#geojson" ).val( geojson );
 	$( "#uuid" ).val( user );
@@ -76,8 +78,23 @@ function delete_neighborhood( name ) {
 }
 
 function add_drawn() {
-	drawn.addLayer( holding );
+	drawn.addLayer( copy_layer( holding ) );
 	$( "#drawn" ).append( '<li class="list-group-item"><span class="glyphicon glyphicon-trash delete"></span><span class="swatch" style="background-color:' + $( "#color" ).val() + '"></span>' + $( "#name-input" ).val() + '</li>' );
+}
+
+function copy_layer( layer ) {
+	var color = $( "#color" ).val(),
+		options = {
+			color : color,
+			fillColor : color
+		};
+		
+	if( layer._radius !== undefined || layer._mRadius !== undefined ) {
+		return L.circle( layer.getLatLng(), layer.getRadius(), options );
+	}
+	else {
+		return L.polygon( layer.getLatLngs(), options );
+	}
 }
 
 function check_vertices() {
