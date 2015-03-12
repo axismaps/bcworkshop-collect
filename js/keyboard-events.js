9494b96ae6d33a9da5902c60320f60e35dc99712
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-03-12 12:45:57 -0500

    React on keyboard presses

--- js/keyboard-events.js
+++ js/keyboard-events.js
@@ -1,10 +1,34 @@
 function keyboard_events(){
+	//arrow keycodes
+	var pan = 80,
+		panKeys = {
+			37 : [ -1 * pan, 0 ], //left
+			39 : [ pan, 0 ], //right
+			40 : [ 0, pan ], //up
+			38 : [ 0, -1 * pan ] //down
+		},
+		zoomKeys = {
+			187 : 1,
+			107 : 1,
+			61 : 1,
+			171 : 1,
+			189 : -1,
+			109 : -1,
+			173 : -1
+		};
+	
 	$( document ).on({
-		keydown: function(e) {
-			var keyCodes = [37,38,39,40],
-				key = e.keyCode;
-			if ( $.inArray( key, keyCodes ) >= 0 ) {
-				console.log('arrow key was pressed');
+		keydown: function( e ) {
+			var key = e.keyCode;
+			if ( key in panKeys ) {
+				map.panBy( panKeys[ key ] );
+				
+				if ( map.options.maxBounds ) {
+					map.panInsideBounds( map.options.maxBounds );
+				}
+			}
+			else if ( key in zoomKeys ) {
+				map.setZoom( map.getZoom() + zoomKeys[ key ] );
 			}
 		}
 	});
