7a202c3034b7b216f9864544e54ab390d6805098
David Heyman <dave@axismaps.com>
2015-03-23 10:07:47 +0000

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 var gulp = require( 'gulp' ),
 	usemin = require( 'gulp-usemin' ),
 	uglify = require( 'gulp-uglify' ),
-	minifyCss = require( 'gulp-minify-css' );
+	minifyCss = require( 'gulp-minify-css' ),
+	del = require( 'del' );
 
-gulp.task( 'default', function(){
-   gulp.src( 'index.html' )
-        .pipe( usemin({
-            assetsDir : '',
-            css : [ minifyCss(), 'concat' ],
-            js : [ uglify(), 'concat' ]
-        }))
-        .pipe( gulp.dest( 'public/' ) )
-    
-    gulp.src( 'img/*' )
-    		.pipe( gulp.dest( 'public/img' ) );
+gulp.task( 'default', [ 'clean' ], function(){
+	gulp.src( 'index.html' )
+		.pipe( usemin({
+			assetsDir : '',
+			css : [ minifyCss(), 'concat' ],
+			js : [ uglify(), 'concat' ]
+		}))
+		.pipe( gulp.dest( 'public/' ) )
+
+	gulp.src( 'img/*' )
+		.pipe( gulp.dest( 'public/img' ) );
     	
-    	gulp.src( 'bower_components/font-awesome/fonts/*' )
-    		.pipe( gulp.dest( 'public/fonts' ) );
+	gulp.src( 'bower_components/fontawesome/fonts/*' )
+		.pipe( gulp.dest( 'public/fonts' ) );
+});
+
+gulp.task( 'clean', function( callback ){
+    del( [ 'public/css', 'public/fonts', 'public/img', 'public/js', 'public/index.html' ], callback );
 });
