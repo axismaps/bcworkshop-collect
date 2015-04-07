1855ad573d6648dc7df42f14715e3581d1fbe739
David Heyman <dave@axismaps.com>
2015-04-07 08:32:25 +0100

    Added district labels

--- js/layers.js
+++ js/layers.js
@@ -1,10 +1,8 @@
-var overlays,
-  labels,
-	layers = [
+var layers = [
 		{
 			"table" : "city_council",
 			"name" : "City Council Districts",
-			"labels" : 'https://{s}.tiles.mapbox.com/v4/bcworkshop12.d564e901/{z}/{x}/{y}.png?access_token=' + accessToken,
+			"labels" : 'https://{s}.tiles.mapbox.com/v4/bcworkshop12.fd1a0343/{z}/{x}/{y}.png?access_token=' + accessToken,
 			"default": false
 		},
 		{
@@ -23,54 +21,3 @@ var overlays,
 			"default" : true
 		}
 	];
-
-function init_layers( button ) {
-	//init layer group to store labels
-	labels = L.layerGroup().addTo( map );
-	
-	//init layer group to store overlays
-	overlays = L.layerGroup().addTo( map );
-	
-	//hides the loader icon on first view
-	$( '#dropdown-toggle i' ).toggle();
-	
-	//button should be a jquery object
-	button.append( '<li role="presentation"><label><input type="radio" name="layers" value="" checked>None</label></li>' );
-	_.each( layers, function( layer ) {
-		var $li = $( '<li role="presentation"><label><input type="radio" name="layers" value="' + layer.table + '"' + ( layer.default ? ' checked="true"' : '' ) +'>' + layer.name + '</label></li>' );
-		$li.data( layer );
-		button.append( $li );
-	});
-	
-	button.find( 'input' ).click( function() {
-		overlays.clearLayers();
-		labels.clearLayers();
-		
-		if( $( this ).val() != '' ) {
-			$( "#dropdown-toggle" ).children().toggle();
-			
-			var layerStyle = L.geoJson( null, {
-				style : function( feature ) {
-					return { 
-						color : '#ed2a24',
-						fillOpacity : 0,
-						pointerEvents : 'none'
-					};
-		    	}
-			});
-			
-			$( "#dropdown-toggle" ).css( "pointer-events", "none" );
-			
-			if( $( this ).parents( "li" ).data().labels ) {
-  				  L.tileLayer( $( this ).parents( "li" ).data().labels ).addTo( labels );
-		  }
-			
-			omnivore.topojson( endpoint + "/topojson/" + $( this ).val(), null, layerStyle ).addTo( overlays ).on( 'ready', function() {
-				$( "#dropdown-toggle" ).children().toggle();
-				$( "#dropdown-toggle" ).css( "pointer-events", "auto" );
-			}); 
-		}
-		map.getContainer().focus();
-	});	
-	button.find( 'input:checked' ).trigger( 'click' );
-}
